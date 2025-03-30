2f81c75d9da45a84656ac3a10eca1aa2bab74f95
Eduardo Carvalho <eduardocarvalhop@protonmail.com>
2025-03-30 20:53:35 +0200

    fix: add carnet de suivi

--- convert-orders/app.py
+++ convert-orders/app.py
@@ -27,18 +27,29 @@ def count(row):
             nb_ga5 = row["quantity"]
             nb_cs = row["quantity"]
 
+    if row["name"].startswith("Carnet du suivi"):
+        nb_cs = row["quantity"]
+
     if row["name"].startswith("Offre cantons"):
         nb_o = row["quantity"]
         nb_ca = row["quantity"]
         nb_cs = row["quantity"]
 
-    return row["order_number"], row["timestamp"], nb_ga4, nb_ga5, nb_ca, nb_cs, nb_o, row["total"]
+    return (
+        row["order_number"],
+        row["timestamp"],
+        nb_ga4,
+        nb_ga5,
+        nb_ca,
+        nb_cs,
+        nb_o,
+        row["total"],
+    )
 
 
 def convert(df):
     df = df.apply(count, axis=1, result_type="expand")
     df.columns = ["name", "date", "GE - A4", "GE - A5", "CA", "CS", "OFFRE", "total"]
-
 
     df = (
         df.groupby(["name", "date"])
@@ -69,7 +80,7 @@ def main():
         products used in those orders.
         """
     )
-    
+
     uploaded_file = st.file_uploader("Upload CSV File", type=["csv"])
     if uploaded_file is not None:
         try:
@@ -77,7 +88,7 @@ def main():
             st.success("Excel file successfully uploaded and processed!")
             ppt_buffer = convert(records)
             excel_buffer = io.BytesIO()
-            ppt_buffer.to_excel(excel_buffer, index=False, engine='openpyxl')
+            ppt_buffer.to_excel(excel_buffer, index=False, engine="openpyxl")
             excel_buffer.seek(0)  # Important: reset buffer position to the beginning
 
             # Provide Excel file for download
@@ -85,7 +96,7 @@ def main():
                 label="Download Converted Orders (Excel)",
                 data=excel_buffer,
                 file_name="converted_orders.xlsx",
-                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
             )
         except Exception as e:
             st.error(f"An error occurred while generating the presentation: {e}")
